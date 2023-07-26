0047333033bd56037e8e356174490fae531cf3bc
Ayrton Laura <ayrtonlauracaja@gmail.com>
2023-07-26 17:56:41 -0500

    New project presupuestos

---
+++ src/app/egreso/egreso.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit ,Input} from '@angular/core';
+import { Egreso } from './egreso.model';
+import { EgresoServicio } from './egreso.servicio';
+
+@Component({
+  selector: 'app-egreso',
+  templateUrl: './egreso.component.html',
+  styleUrls: ['./egreso.component.css']
+})
+export class EgresoComponent implements OnInit {
+
+  egresos: Egreso[]=[];
+  @Input() ingresoTotal:number;
+  constructor(private egresoService: EgresoServicio) { }
+
+  ngOnInit(): void {
+    this.egresos= this.egresoService.egresos;
+  }
+
+  eliminarEgreso(egreso: Egreso){
+    this.egresoService.eliminar(egreso);
+  }
+
+  calcularPorcentaje(egreso: Egreso){
+    return egreso.valor/this.ingresoTotal;
+  }
+}
